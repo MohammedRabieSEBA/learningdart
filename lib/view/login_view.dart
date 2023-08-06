7e62be0d52ec1cd728d952ba750b885c504bf067
Mohammed Rabie <125748857+MohammedRabieSEBA@users.noreply.github.com>
2023-08-06 14:18:09 +0100

    step-04

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -61,6 +61,9 @@ class _LoginViewState extends State<LoginView> {
                   password: password,
                 );
                 devtools.log(userCredential.toString());
+                // ignore: use_build_context_synchronously
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/notes/', (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   // Handle the case when the user is not found
